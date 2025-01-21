2e2678af8e3f57a679568a4e9a287d0b94bd6b8f
Philip Nilsson <163206076+Phnilssonux@users.noreply.github.com>
2025-01-21 18:05:35 +0100

    Create app.ts

---
+++ 02week01lesson/app.ts
@@ -0,0 +1,31 @@
+// Initial count value
+let count: number=0;
+
+// Update the count display
+const updateCountDisplay = () =>  {
+    const countElement: any = document.getElementById('count');
+    countElement.textContent = count;
+};
+
+// Increment the count
+const increment = () =>  {
+    count += 1;
+    updateCountDisplay();
+};
+
+// Decrement the count
+const decrement = () => {
+    count -= 1;
+    updateCountDisplay();
+};
+
+// Reset the count
+const reset = () => {
+    count = 0;
+    updateCountDisplay();
+};
+
+// Attach event listeners to buttons
+(document.getElementById('increment-button')as HTMLElement).onclick = increment;
+(document.getElementById('decrement-button')as HTMLElement).onclick = decrement;
+(document.getElementById('reset-button')as HTMLElement).onclick = reset;
